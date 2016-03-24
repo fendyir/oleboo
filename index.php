0b635eacf4597d3bd563b39c37450380a3ad76ce
fendyir <f.irwansyah@ymail.com>
2016-03-23 23:13:54 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Oleboo</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+</head>
+<body id="top">
+<div class="wrapper col1">
+  <div id="topbar">  </div>
+</div>
+<div class="wrapper col2">
+  <div id="header">
+    <div class="fl_left">
+      <h1>Oleboo</h1>
+      <p>Informasi Lokasi Oleh-Oleh Bogor</p>
+    </div>
+    <ul id="topnav">
+      <li><a href="kontak.html">Kontak</a></li>
+      <li><a href="toko.html">Toko Oleh-Oleh</a></li>
+      <li><a href="galeri.html">Galeri</a></li>
+      <li><a href="profil.html">Profil</a></li>
+      <li class="active"><a href="index.php">Beranda</a></li>
+    </ul>
+    <br class="clear" />
+  </div>
+</div>
+<div class="wrapper col3"></div>
+<div class="wrapper col4">
+  <div id="container">
+    <div id="content">
+      <h1>Selamat Datang di Sistem Informasi Oleh-oleh Khas Bogor</h1>
+      <div class="imgholder"><img src="images/b1.jpg" width="600" height="283" alt="" /></div>
+      <p align="justify">Ketika berkunjung ke kota bogor, setelah lelah berwisata alam, berwisata kuliner ke tempat-tempat yang bagus di Bogor maka kurang lengkap jika tidak membawa buah tangan atau oleh-oleh untuk keluarga atau kerabat di rumah. Bogor mempunyai sumber daya alam yang melimpah karena letaknya yang dikelilingi pegunungan dan berada di dataran tinggi maka tak heran biasanya oleh-oleh yang dibawa dari bogor biasanya berupa olahan makanan dari sumber daya alam yang ada di bogor seperti buah-buahan atau sayur-sayuran yang diubah menjadi makanan yang sangat lezat. </p>
+      <h2>&nbsp;</h2>
+      <div id="respond"></div>
+    </div>
+    <div id="column">
+      <div class="holder">
+        <h2 class="title"><strong>10 Oleh-oleh Kuliner dari Kota Bogor</strong></h2>
+        <p>Ketika berkunjung ke kota bogor, setelah lelah berwisata alam, berwisata  kuliner ke tempat-tempat yang bagus di Bogor maka kurang lengkap jika tidak  membawa buah tangan atau oleh-oleh untuk keluarga atau kerabat di rumah.</p>
+        <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+      </div>
+      </div>
+      <div id="column">
+      <div class="holder">
+        <h2 class="title"><strong>Oleh-oleh Khas Bogor yang Paling Terkenal</strong></h2>
+        <p>Ketika berkunjung ke kota bogor, setelah lelah berwisata alam, berwisata  kuliner ke tempat-tempat yang bagus di Bogor maka kurang lengkap jika tidak  membawa buah tangan atau oleh-oleh untuk keluarga atau kerabat di rumah.</p>
+        <p class="readmore"><a href="#">Selengkapnya &raquo;</a></p>
+      </div>
+      </div>
+      
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="wrapper col5"></div>
+<div class="wrapper col6">
+  <div id="copyright">
+    <p class="fl_left">Copyright &copy; 2015  - oleboo.com</p>
+    <br class="clear" />
+  </div>
+</div>
+</body>
+</html>
